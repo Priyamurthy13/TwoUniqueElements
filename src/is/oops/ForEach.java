b79e57a4ca7de59cf015503d981f2834026188d0
Priyamurthy13 <murthypriyanka1@gmail.com>
2026-02-20 13:08:49 +0530

    Initial commit

---
+++ src/is/oops/ForEach.java
@@ -0,0 +1,16 @@
+package is.oops;
+
+public class ForEach {
+    public static void main(String[] args) {
+        char[][] names = {{'d','e','e', 'p','t', 'i'},
+                {'p','r', 'a','n', 'a', 'v'},
+                {'s', 'u', 'r','y','a','s'}};
+        for(int r=0; r<names.length;r++){
+            for(int c=0; c<6;c++){
+                System.out.print(names[r] [c]);
+            }
+            System.out.println();
+        }
+    }
+}
+
---
+++ src/methods/SwapUsingArray.java
@@ -0,0 +1,17 @@
+package methods;
+
+public class SwapUsingArray {
+    static void swap(int[] arr){
+       int temp=arr[0];
+       arr[0]=arr[1];
+       arr[1]=temp;
+    }
+    public static void main(String[] args){
+        int x=10,y=20;
+        int[] arr={x,y};
+        swap(arr);
+        x=arr[0];
+        y=arr[1];
+        System.out.println(x+","+y);
+    }
+}
